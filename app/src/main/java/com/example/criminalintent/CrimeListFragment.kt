a97eaf04fcea9a61d15385540e5f9ba9628ad219
Dennis <denikliv@gmail.com>
2022-11-11 18:59:37 -0800

    v4 redo

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -7,10 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.criminalintent.databinding.FragmentCrimeListBinding
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
-private const val TAG = "CrimeListFragment"
+
 class CrimeListFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -22,16 +27,6 @@ class CrimeListFragment : Fragment() {
         }
 
     private val crimeListViewModel: CrimeListViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
-    }
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -51,6 +46,21 @@ class CrimeListFragment : Fragment() {
 
 
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                val crimes = crimeListViewModel.loadCrimes()
+                binding.crimeRecyclerView.adapter =
+                    CrimeListAdapter(crimes)
+            }
+
+
+        }
+    }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
